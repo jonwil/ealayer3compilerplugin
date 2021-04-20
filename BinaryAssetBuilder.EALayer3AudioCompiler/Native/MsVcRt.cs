ca158c3dc416f154f62c39f0f46b351e2df9884c
Jonathan Wilson <jfwfreo@tpgi.com.au>
2021-04-20 17:27:52 +1000

    Somehow this didn't get committed properly

---
+++ BinaryAssetBuilder.EALayer3AudioCompiler/Native/MsVcRt.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Native
+{
+    public static class MsVcRt
+    {
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate IntPtr MemSetDelegate(IntPtr ptr, byte value, SizeT count);
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate int MemICmpDelegate([In] IntPtr buf1, [In] IntPtr buf2, SizeT size);
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate IntPtr MemCpyDelegate(IntPtr dest, [In] IntPtr src, SizeT count);
+        [UnmanagedFunctionPointer(CallingConvention.FastCall)]
+        public delegate SizeT StrLenDelegate([In] IntPtr str);
+
+        private const string _moduleName = "msvcrt.dll";
+
+        private static readonly IntPtr _hModule;
+
+        public static readonly MemSetDelegate MemSet;
+        public static readonly MemICmpDelegate MemICmp;
+        public static readonly MemCpyDelegate MemCpy;
+
+        static MsVcRt()
+        {
+            _hModule = NativeLibrary.Load(_moduleName);
+            MemSet = (MemSetDelegate)Marshal.GetDelegateForFunctionPointer(NativeLibrary.GetExport(_hModule, "memset"), typeof(MemSetDelegate));
+            MemICmp = (MemICmpDelegate)Marshal.GetDelegateForFunctionPointer(NativeLibrary.GetExport(_hModule, "_memicmp"), typeof(MemICmpDelegate));
+            MemCpy = (MemCpyDelegate)Marshal.GetDelegateForFunctionPointer(NativeLibrary.GetExport(_hModule, "memcpy"), typeof(MemCpyDelegate));
+        }
+    }
+}
---
+++ BinaryAssetBuilder.EALayer3AudioCompilerTW/Native/NativeLibrary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Native
+{
+    public static class NativeLibrary
+    {
+        public static IntPtr Load(string libraryPath)
+        {
+            return Kernel32.Load(libraryPath);
+        }
+
+        public static IntPtr GetExport(IntPtr handle, string name)
+        {
+            return Kernel32.GetExport(handle, name);
+        }
+
+        public static bool Close(IntPtr handle)
+        {
+            return Kernel32.Close(handle);
+        }
+    }
+}
